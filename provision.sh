2c78f3d7905cf6841c2b6f8b3ff257144ef9cf47
Neivin Mathew <me@neivin.com>
2018-06-27 16:18:02 -0700

    provisioning logic

--- provision.sh
+++ provision.sh
@@ -2,11 +2,9 @@
 
 # Provision a new machine
 # Author: Neivin Mathew (me@neivin.com)
+# Date: June 27, 2018
 
-# Check if git is installed
-if [ -x "$(command -v git)" ]
-  echo 'ERROR: Git is not installed.' >&2
-  exit 1
-fi
+# Get the path of the current file and copy to ~/dotfiles
 
-# pull the repo
+
+# Append the source line to the end of .bash_profile and .bashrc
